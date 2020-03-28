92e1f8841db33283a4f3f9b0941a2c979f60d8d7
Juan Torres <jet@Juans-MBP.home>
2020-03-27 21:36:34 -0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+module.exports = app;
+
+app.use(express.json());
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`
+      http://localhost:${PORT}
+
+
+   `);
+});
