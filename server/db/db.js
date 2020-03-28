ca79697613f27e708c3d4a823fe02b639b9fbaa6
Juan Torres <jet@Juans-MBP.home>
2020-03-28 01:58:28 -0400

    added db.js

---
+++ server/db/db.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/soloGraceShopper',
+  { logging: false }
+);
+
+module.exports = db;
