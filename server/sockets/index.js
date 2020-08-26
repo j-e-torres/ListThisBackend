e44811401dae06b54fe7fdf16a62e7564450b2e7
Juan Torres <juanet1453@gmail.com>
2020-08-26 04:35:38 -0400

    added socket user

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,7 +1,8 @@
 module.exports = (socketIO) => {
   socketIO.on('connection', (socket) => {
-    console.log('socket connected');
-    socket.emit('errrar');
+    socket.on('list-added-user', (user) => {
+      socket.emit('list-added-user', user);
+    });
 
     socket.on('new-lists', (lists) => {
       socket.emit('new-lists', lists);
